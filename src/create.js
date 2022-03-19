e1d9e1096407804927dd263a8873c30c7b6a01d5
stone <stone.grandia@163.com>
2022-03-19 15:58:46 +0800

    create vue3 ssr

--- src/create.js
+++ src/create.js
@@ -1,10 +1,23 @@
-module.exports = function (name) {
-    console.info('start create', name, 'project ...')
-    return new Promise(resolve => {
-        setTimeout(() => {
-            console.info('mission comlete.')
-            resolve()
-        }, 3000);
-    })
+const { HttpApi } = require('@cogears/http-client')
+const decompress = require('decompress');
 
+const url = 'https://github.com/cogears/vue3-ssr/archive/refs/heads/main.zip'
+
+async function run(name) {
+    console.info('下载项目...')
+    let buffer = await new HttpApi().get(url)
+    console.info('部署项目...')
+    await decompress(buffer, name)
+    console.info('部署完毕。')
+}
+
+module.exports = function (name) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            run(name)
+            resolve()
+        } catch (err) {
+            reject(err)
+        }
+    })
 }
